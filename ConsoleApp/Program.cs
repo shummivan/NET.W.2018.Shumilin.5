e82af0d9df483bcb1d5785135e4be59250c34f74
Иван Шумилин <shuma-98@hotmail.com>
2018-10-21 18:06:50 +0300

    Tasks day 5

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polynomial;
+using BubbleSort;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            My_Polynomial p1 = new My_Polynomial(4, 7, 5);
+            My_Polynomial p2 = new My_Polynomial(1, 2, 1, 1);
+
+            //Console.WriteLine((p1 - p2).ToString());
+
+            My_BubbleSort bs = new My_BubbleSort();
+
+            int[][] arr = new int[][]
+            {
+                new int[] {10,-50,8,9},
+                new int[]{3,5,6 },
+                new int[] {0,0,0,0},
+                new int[]{1,0,0,2},
+                new int[] {4,-5,-6,7}        
+            };
+
+            bs.SortByRegrowthSumRows(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            bs.SortByRegrowthMaxElementRows(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            bs.SortByRegrowthMinElementRows(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polynomial.Tests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Polynomial.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(1, 2, 3, 4, ExpectedResult = "+4x^3+3x^2+2x^1+1=0")]
+        public string toString_Polinom_PolynomString(params double[] coeff)
+        {
+            My_Polynomial a = new My_Polynomial(coeff);
+            return a.ToString();
+        }
+
+        [TestCase(ExpectedResult = "+3x^3+5x^2+9x^1+9=0")]
+        public string OperatorSum_Polinom_PolynomString()
+        {
+            My_Polynomial a = new My_Polynomial(4, 7, 5);
+            My_Polynomial b = new My_Polynomial(5, 2, 0, 3);
+
+            return (a + b).ToString();
+        }
+
+        [TestCase(ExpectedResult = "+21x^5+32x^4+34x^3+28x^2+13x^1+4=0")]
+        public string OperatorMult_Polinom_PolynomString()
+        {
+            My_Polynomial a = new My_Polynomial(1, 2, 3);
+            My_Polynomial b = new My_Polynomial(4, 5, 6, 7);
+
+            return (a * b).ToString();
+        }
+
+        [TestCase(ExpectedResult = "-3x^3+5x^2+5x^1-1=0")]
+        public string OperatorSubstruct_Polinom_PolynomString()
+        {
+            My_Polynomial a = new My_Polynomial(4, 7, 5);
+            My_Polynomial b = new My_Polynomial(5, 2, 0, 3);
+            My_Polynomial c = a - b;
+            return c.ToString();
+        }
+    }
+}
